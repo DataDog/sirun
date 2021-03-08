1b6afff33cf6ebaa69cfa24f84b9575f4d8bd245
Bryan English <bryan.english@datadoghq.com>
2021-03-08 16:52:18 -0500

    automated tests

---
+++ tests/examples.rs
@@ -0,0 +1,77 @@
+use assert_cmd;
+use predicates::prelude::*;
+use serial_test::serial;
+
+macro_rules! run {
+    ($file:expr) => {
+        assert_cmd::Command::cargo_bin("sirun").unwrap().arg($file)
+    };
+}
+
+#[test]
+#[serial]
+fn simple() {
+    run!("examples/simple.json").assert().success();
+}
+
+#[test]
+#[serial]
+fn simple_name() {
+    run!("examples/simple.json")
+        .env("SIRUN_NAME", "test test")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("\"name\": \"test test\""));
+}
+
+#[test]
+#[serial]
+fn simple_version() {
+    run!("examples/simple.json")
+        .env("GIT_COMMIT_HASH", "123abc")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("\"version\": \"123abc\""));
+}
+
+#[test]
+#[serial]
+fn no_setup() {
+    run!("examples/no-setup.json").assert().success();
+}
+
+#[test]
+#[serial]
+fn variants() {
+    run!("./examples/variants.json")
+        .env("SIRUN_VARIANT", "0")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("variant 0"));
+    run!("./examples/variants.json")
+        .env("SIRUN_VARIANT", "1")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("variant 1"));
+}
+
+#[test]
+#[serial]
+fn timeout() {
+    run!("examples/timeout.json").assert().failure();
+}
+
+#[test]
+#[serial]
+fn env() {
+    run!("./examples/env.json")
+        .env("SIRUN_VARIANT", "0")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("something zero"));
+    run!("./examples/env.json")
+        .env("SIRUN_VARIANT", "1")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("something one"));
+}
